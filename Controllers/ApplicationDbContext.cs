5f69b8aeb1f102c75c592270d7924811bfccb2e4
nouf <nouf@DESKTOP-GCGMIF9>
2024-07-30 01:20:07 +0300

    Add project files.

---
+++ Controllers/ApplicationDbContext.cs
@@ -0,0 +1,6 @@
+﻿namespace Products.Controllers
+{
+    internal class ApplicationDbContext
+    {
+    }
+}
---
+++ Models/Damagedproducts.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Products.Models
+{
+    public class Damagedproducts
+    {
+
+        [Key]
+        public int Id { get; set; }
+        [Required]
+
+        //[ForeignKey("Product")]
+        public int Products_Id { get; set; }
+        [Required]
+        public int Qty { get; set; }
+
+    }
+}
